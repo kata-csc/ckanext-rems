1823a8ebcacac7d991e3403bc0ba784a9b62aea2
Mika Wahlroos <mika.wahlroos@csc.fi>
2014-02-10 14:25:00 +0200

    Add REMS resource domain and dataset owner to posted metadata

--- ckanext/rems/rems_client.py
+++ ckanext/rems/rems_client.py
@@ -3,19 +3,25 @@ Methods for communicating with the Resource Entitlement Management System
 """
 
 import json
+import logging
+import settings
+
+log = logging.getLogger("ckanext.rems.rems_client")
 
 def generate_json_metadata(pkg_dict):
     metadata = {}
     metadata['titles'] = pkg_dict['langtitle']
 
     resource = {}
-    resource['resourceDomain'] = '?'
-    resource['resourceId'] = pkg_dict['id']
+    resource['resourceDomain'] = settings.REMS_RESOURCE_DOMAIN
+    resource['resourceId'] = pkg_dict['name']
 
     if 'resources' in pkg_dict and len(pkg_dict['resources']) > 0:
         resource['resourceUrl'] = pkg_dict['resources'][0]['url']
     metadata['resource'] = resource
 
-    # FIXME: package rest of the metadata
+    metadata['resource']['owners'] = [ { 'email': pkg_dict['maintainer_email'] } ]
+
+    # FIXME: license metadata
 
     return json.dumps({ 'simplecatalogitem': metadata })
